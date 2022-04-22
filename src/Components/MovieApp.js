7d51a6e0fbd4b067d1f0253c07303bb613340906
Harshit Saini <harshitsaini3582@gmail.com>
2022-04-22 12:15:32 +0530

    api changes

--- src/Components/MovieApp.js
+++ src/Components/MovieApp.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputBox from './InputBox'
 import MoviesTable from './MoviesTable';
 import Pagination from "./Pagination";
 
 function Movies() {
+ let [searchText,setSearchText]=useState("");
+  
+ const setGlobalSearchText=(searchText)=>{
+  setSearchText(searchText);
+ }
+
   return (<div >
-    <InputBox></InputBox>
-    <MoviesTable></MoviesTable>
+    <InputBox setGlobalText={setGlobalSearchText} ></InputBox>
+    <MoviesTable searchText={searchText}></MoviesTable>
     <Pagination></Pagination>
   </div>
     )
